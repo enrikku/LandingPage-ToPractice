1d32a2accd3d208aba11c35f8668a952eaef1d4f
enrikku <enric.cancru@gmail.com>
2024-05-06 22:21:17 +0200

    A

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,9 +2,28 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundColor: {
+				'supabase': '#43cc8e',
+				'angular': '#de002d',
+				'postgres': '#2f6792',
+				'tailwind': '#16becb',
+				'node': '#74b956',
+			},
+			borderColor: {
+				'supabase': '#43cc8e',
+				'angular': '#de002d',
+				'postgres': '#2f6792',
+				'tailwind': '#16becb',
+				'node': '#74b956',
+			},
+		},
+	},
+	variants: {
+		fill: ['hover', 'focus'],
 	},
 	plugins: [
 		require('daisyui'),
+		require('tailwindcss-animated')
 	],
 }
